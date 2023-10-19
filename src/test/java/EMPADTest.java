cd218eab5da1ba48158c963201466d10eba0357b
sharifzadeh <amirsharifzadeh@gmail.com>
2023-10-19 11:25:17 -0400

    add yml

---
+++ src/test/java/EMPADTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class EMPADTest {
+
+
+    @Test
+    void addition() {
+        assertEquals(2, sum(1, 1));
+    }
+
+    private int sum(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+    }
+}
